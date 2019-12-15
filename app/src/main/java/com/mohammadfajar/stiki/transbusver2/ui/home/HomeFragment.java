aabda2a4dfa880e6677e63a5c90c11bafdebe6fd
galihsatriawan <satriawan.galih99@gmail.com>
2019-12-15 10:59:30 +0700

    - Fixing Image slider in home fragment

--- app/src/main/java/com/mohammadfajar/stiki/transbusver2/ui/home/HomeFragment.java
+++ app/src/main/java/com/mohammadfajar/stiki/transbusver2/ui/home/HomeFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -15,7 +17,7 @@ import androidx.lifecycle.ViewModelProviders;
 import com.mohammadfajar.stiki.transbusver2.R;
 
 public class HomeFragment extends Fragment {
-
+    ViewFlipper v_flipper;
     private HomeViewModel homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -23,13 +25,26 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        int images[] = {R.drawable.slide1, R.drawable.slide2, R.drawable.slide3};
+        v_flipper = root.findViewById(R.id.v_flipper);
+
+        for (int i =0; i<images.length; i++){
+            fliverImages(images[i]);
+        }
+        for (int image: images)
+            fliverImages(image);
         return root;
+    }
+    public  void  fliverImages(int images){
+        ImageView imageView = new ImageView(getContext());
+        imageView.setBackgroundResource(images);
+
+        v_flipper.addView(imageView);
+        v_flipper.setFlipInterval(4000);
+        v_flipper.setAutoStart(true);
+
+        v_flipper.setInAnimation(getContext(),android.R.anim.slide_in_left);
+        v_flipper.setOutAnimation(getContext(),android.R.anim.slide_out_right);
+
     }
 }
